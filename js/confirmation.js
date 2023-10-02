60b5fd45a3d8d401482ef3a3892672397efc75fc
Nathan Akpawu <nakpawu@gmail.com>
2023-10-02 16:53:41 +0000

    send confirmation form

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,7 +1,12 @@
 'use strict';
 
-const confirmationGroupEl = document.getElementById('confirmation-group');
+const phoneNumberID = '134210913111335';
+const userAccessToken =
+    'EAAPJg368hmsBOZCrCwapqJdp3eSjVGioiKOpIwBJ2QZBjz1eSrljy9gqFxOGDZBSB4vlnww9y2AG43Yvr5sKVByH5YZAk1B1eUCegku2M4LsMDTHw6gmeXCITVBy0P7o9ytrem8XLfJ0dZBhbcYA31YjQ8EjljZBJr8F8TUgOab0OT9d0Vm6f5UnhYHG2fgRkI';
+const recipientPhoneNumber = '22892145161';
+const version = 'v18.0';
 
+const confirmationGroupEl = document.getElementById('confirmation-group');
 const confirmationFormEl = document.getElementById('confirmation-form');
 const nameInput = document.getElementById('name');
 const guestSelect = document.getElementById('guest');
@@ -29,30 +34,36 @@ function displayAlertError() {
 
 confirmationFormEl.addEventListener('submit', event => {
     event.preventDefault();
+
     const name = nameInput.value;
     const guest = guestSelect.value;
     const datas = {
         messaging_product: 'whatsapp',
         recipient_type: 'individual',
-        to: '22892145161',
+        to: recipientPhoneNumber,
         type: 'text',
         text: {
             body: `*Confirmation de présence pour Royal Beach*\nNom : ${name}\nNombre de personnes : ${guest}`,
         },
     };
+
+    displaySpinner();
     postConfirmationForm(datas);
 });
 
 function postConfirmationForm(datas) {
-    fetch('https://graph.facebook.com/v18.0/134210913111335/messages', {
+    fetch(`https://graph.facebook.com/${version}/${phoneNumberID}/messages`, {
         method: 'POST',
         headers: {
-            Authorization:
-                'Bearer EAAPJg368hmsBOZCrCwapqJdp3eSjVGioiKOpIwBJ2QZBjz1eSrljy9gqFxOGDZBSB4vlnww9y2AG43Yvr5sKVByH5YZAk1B1eUCegku2M4LsMDTHw6gmeXCITVBy0P7o9ytrem8XLfJ0dZBhbcYA31YjQ8EjljZBJr8F8TUgOab0OT9d0Vm6f5UnhYHG2fgRkI',
+            Authorization: `Bearer ${userAccessToken}`,
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(datas),
-    }).then(response => {
-        console.log(response);
-    });
+    })
+        .then(response => {
+            response.ok ? displayAlertSuccess() : displayAlertError();
+        })
+        .catch(error => {
+            throw new Error(error);
+        });
 }
